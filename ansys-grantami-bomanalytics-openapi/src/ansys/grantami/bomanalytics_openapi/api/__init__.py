71684d6ebea93fdd16234decf0c5ce629d8ebf4d
Andy-Grigg <Andy-Grigg@users.noreply.github.com>
2023-05-24 15:35:42 +0000

    Generate Client Library code - Automated

---
+++ ansys-grantami-bomanalytics-openapi/src/ansys/grantami/bomanalytics_openapi/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import API ABC
+from ansys.openapi.common import ApiBase
+
+# import apis into api package
+from .compliance_api import ComplianceApi
+from .documentation_api import DocumentationApi
+from .impacted_substances_api import ImpactedSubstancesApi
