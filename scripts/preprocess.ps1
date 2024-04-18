d070da2d3cc16a84826493253087c4912fd50299
Andy Grigg <andrew.grigg@ansys.com>
2024-04-18 08:09:28 -0400

    Add pre-commit config and run in CI (#131)
    * Add and run pre-commit
    
    * Allow 'failure'
    
    * Generate Client Library code - Automated
    
    * Move config to repo files
    
    * Generate Client Library code - Automated
    
    * Fix yaml
    
    * Generate Client Library code - Automated
    
    ---------
    
    Co-authored-by: Andy-Grigg <Andy-Grigg@users.noreply.github.com>

--- scripts/preprocess.ps1
+++ scripts/preprocess.ps1
@@ -1,19 +1,19 @@
- param (
+param (
 	[Parameter(Mandatory=$true)]
 	[string] $OutputPath
- )
+)
 
 
 function Edit-YamlFile {
 	param (
 		[string]$OutputPath
 	)
-	
+
 	(Get-Content $OutputPath) | Foreach-Object {
 		$_ -replace '\s?(\\r)?\\n\s+', ' ' `
 		   -replace '(\\\\\s*)+', ' ' `
 		} | Set-Content $OutputPath
-	
+
 }
 
 Edit-YamlFile $OutputPath
